2e95b2ad5f41627c5563f021872dcf0024bde365
Michael Borczuk <michaelborczuk@gmail.com>
2025-04-28 00:30:09 -0400

    model

--- logs/read-data.py
+++ logs/read-data.py
@@ -1,6 +1,7 @@
 import json
+import numpy as np 
 
-f = open("test.txt")
+f = open("logs\\test.txt")
 l = f.readlines()
 g = open("samples.json", "w")
 
@@ -26,8 +27,20 @@ while (index < len(l)):
     if (rot < 0):
         rot = 3 - (rot + 1)
     print(move_list, step, rot)
+    board = l[index + 1][0:-1]
+    board = json.loads(board)
+    power = len(board[0]) * len(board) - 1
+    print(power)
+    sum = 0
+    for i in board:
+        for j in i:
+            if (j != 0):
+                sum += 2 ** power
+            power -= 1
+    print(sum)
+    print(len(str(sum)))
     dic[sample_count] = {}
-    dic[sample_count]["board"] = l[index + 1][0:-1]
+    dic[sample_count]["board"] = sum
     dic[sample_count]["piece"] = l[index][0:-1]
     dic[sample_count]["movement"] = step
     dic[sample_count]["rotation"] = rot
