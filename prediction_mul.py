80464db0765e5fdff658a8e5aa7109facd8e13b0
Melvin Aziz <58566746+MelvAziz@users.noreply.github.com>
2025-04-29 02:07:14 -0400

    Slightly better trenchcoat

--- prediction_mul.py
+++ prediction_mul.py
@@ -6,12 +6,16 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import json
 
 
 def predict(data):
     f = open("samples_new_2.json")
     j = json.loads("\n".join(f.readlines()))
+    f2 = open("data_new_1.json")
+    j2 = json.loads("\n".join(f2.readlines()))
     # Example using NumPy arrays
     x_1 = []
     y_1 = []
@@ -39,9 +43,9 @@ def predict(data):
         elif(j[str(i)]['piece'] == "S"):
             x_2.append(sample_x)
             y_2.append(sample_y)
-        elif(j[str(i)]['piece'] == "J"):
-            x_3.append(sample_x)
-            y_3.append(sample_y)
+        # elif(j[str(i)]['piece'] == "J"):
+        #     x_3.append(sample_x)
+        #     y_3.append(sample_y)
         elif(j[str(i)]['piece'] == "I"):
             x_4.append(sample_x)
             y_4.append(sample_y)
@@ -51,59 +55,109 @@ def predict(data):
         elif(j[str(i)]['piece'] == "Z"):
             x_6.append(sample_x)
             y_6.append(sample_y)
-        elif(j[str(i)]['piece'] == "T"):
+    for i in j2.keys():
+        sample_x = j2[str(i)]['board']
+        # for k in range(0, 20):
+        #     sample_x.append(int(j[str(i)]['board'][k]))
+        sample_y = f"{j2[str(i)]['movement']}:{j2[str(i)]['rotation']}"
+        if(j2[str(i)]['piece'] == "J"):
+            x_3.append(sample_x)
+            y_3.append(sample_y)
+        elif(j2[str(i)]['piece'] == "T"):
             x_7.append(sample_x)
             y_7.append(sample_y)
-    # scaler = MinMaxScaler()
+    scaler = MinMaxScaler()
 
     X_1_np = np.array(x_1)
     y_1_np = np.array(y_1)
-    # scaled_data_1 = scaler.fit_transform(X_1_np)
+    scaled_data_1 = scaler.fit_transform(X_1_np)
+    X_train_1, X_test_1, y_train_1, y_test_1 = train_test_split(scaled_data_1, y_1_np, random_state=42)
+    # model_1 =  MLPClassifier(solver='sgd', hidden_layer_sizes=(32, 16),
+    #                 max_iter=1000, random_state=42)
     # X_train_1, X_test_1, y_train_1, y_test_1 = train_test_split(scaled_data_1, y_1_np, random_state=42)
-    X_train_1, X_test_1, y_train_1, y_test_1 = train_test_split(X_1_np, y_1_np, random_state=42)
-    model_1 =  RandomForestClassifier()
+    model_1 = KNeighborsClassifier(9)
     model_1.fit(X_train_1, y_train_1)
+    y_pred_1 = model_1.predict(X_test_1);
+    accuracy = accuracy_score(y_test_1, y_pred_1)
+    print(f"Accuracy 1: {accuracy*100:.2f}%")
 
     X_2_np = np.array(x_2)
     y_2_np = np.array(y_2)
-    X_train_2, X_test_2, y_train_2, y_test_2 = train_test_split(X_2_np, y_2_np, random_state=42)
-    model_2 =  RandomForestClassifier()
+    scaled_data_2 = scaler.fit_transform(X_2_np)
+    X_train_2, X_test_2, y_train_2, y_test_2 = train_test_split(scaled_data_2, y_2_np, random_state=42)
+    model_2 = RandomForestClassifier(n_estimators=9)
     model_2.fit(X_train_2, y_train_2)
+    y_pred_2 = model_2.predict(X_test_2);
+    accuracy = accuracy_score(y_test_2, y_pred_2)
+    print(f"Accuracy 2: {accuracy*100:.2f}%")
 
     X_3_np = np.array(x_3)
     y_3_np = np.array(y_3)
-    X_train_3, X_test_3, y_train_3, y_test_3 = train_test_split(X_3_np, y_3_np, random_state=42)
-    model_3 =  RandomForestClassifier()
+    scaled_data_3 = scaler.fit_transform(X_3_np)
+    X_train_3, X_test_3, y_train_3, y_test_3 = train_test_split(scaled_data_3, y_3_np, random_state=42)
+    model_3 =   MLPClassifier(hidden_layer_sizes=(62, 4),
+                    max_iter=1000, random_state=42)
+    # model_3 = KNeighborsClassifier(6)
     model_3.fit(X_train_3, y_train_3)
+    y_pred_3 = model_3.predict(X_test_3);
+    accuracy = accuracy_score(y_test_3, y_pred_3)
+    print(f"Accuracy 3: {accuracy*100:.2f}%")
 
     X_4_np = np.array(x_4)
     y_4_np = np.array(y_4)
-    X_train_4, X_test_4, y_train_4, y_test_4 = train_test_split(X_4_np, y_4_np, random_state=42)
-    model_4 =  RandomForestClassifier()
+    scaled_data_4 = scaler.fit_transform(X_4_np)
+    X_train_4, X_test_4, y_train_4, y_test_4 = train_test_split(scaled_data_4, y_4_np, random_state=42)
+    model_4 = KNeighborsClassifier(6)
+    # model_4 = KNeighborsClassifier(3)
     model_4.fit(X_train_4, y_train_4)
+    y_pred_4 = model_4.predict(X_test_4);
+    accuracy = accuracy_score(y_test_4, y_pred_4)
+    print(f"Accuracy 4: {accuracy*100:.2f}%")
 
     X_5_np = np.array(x_5)
     y_5_np = np.array(y_5)
-    X_train_5, X_test_5, y_train_5, y_test_5 = train_test_split(X_5_np, y_5_np, random_state=42)
-    model_5 = RandomForestClassifier()
+    scaled_data_5 = scaler.fit_transform(X_5_np)
+    X_train_5, X_test_5, y_train_5, y_test_5 = train_test_split(scaled_data_5, y_5_np, random_state=42)
+    # model_5 = RandomForestClassifier(n_estimators=6)
+    # model_5 = MLPClassifier(hidden_layer_sizes=(32, 16),
+    #                 max_iter=1000, random_state=42)
+    model_5 = KNeighborsClassifier(6)
     model_5.fit(X_train_5, y_train_5)
+    y_pred_5 = model_5.predict(X_test_5);
+    accuracy = accuracy_score(y_test_5, y_pred_5)
+    print(f"Accuracy 5: {accuracy*100:.2f}%")
 
     X_6_np = np.array(x_6)
     y_6_np = np.array(y_6)
-    X_train_6, X_test_6, y_train_6, y_test_6 = train_test_split(X_6_np, y_6_np, random_state=42)
-    model_6 =  RandomForestClassifier()
+    scaled_data_6 = scaler.fit_transform(X_6_np)
+    X_train_6, X_test_6, y_train_6, y_test_6 = train_test_split(scaled_data_6, y_6_np, random_state=42)
+    # model_6 = GaussianNB()
+    # model_6 =  MLPClassifier(hidden_layer_sizes=(32, 16),
+    #                 max_iter=1000, random_state=42)
+    model_6 = KNeighborsClassifier(11)
     model_6.fit(X_train_6, y_train_6)
+    y_pred_6 = model_6.predict(X_test_6);
+    accuracy = accuracy_score(y_test_6, y_pred_6)
+    print(f"Accuracy 6: {accuracy*100:.2f}%")
 
     X_7_np = np.array(x_7)
     y_7_np = np.array(y_7)
-    X_train_7, X_test_7, y_train_7, y_test_7 = train_test_split(X_7_np, y_7_np, random_state=42)
-    model_7 =  RandomForestClassifier()
+    scaled_data_7 = scaler.fit_transform(X_7_np)
+    X_train_7, X_test_7, y_train_7, y_test_7 = train_test_split(scaled_data_7, y_7_np, random_state=42)
+    # model_7 = KNeighborsClassifier(20)
+    model_7 =  MLPClassifier(activation="relu", solver='adam', hidden_layer_sizes=(8, 3),
+                    max_iter=5000, random_state=42)
+    # model_7 = RandomForestClassifier()
     model_7.fit(X_train_7, y_train_7)
+    y_pred_7 = model_1.predict(X_test_7);
+    accuracy = accuracy_score(y_test_7, y_pred_7)
+    print(f"Accuracy 7: {accuracy*100:.2f}%")
 
 
     board = str(data[0])
     board = json.loads(board)
-    # power = len(board[0]) * len(board) - 1
+    power = len(board[0]) * len(board) - 1
+
     sum_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     row = 0;
@@ -114,6 +168,22 @@ def predict(data):
                 sum_list[loc] = 20-row;
             loc+=1;
         row+=1;
+
+
+
+    # sum_list = []
+
+    # for i in board:
+    #     sum = 0
+    #     power = len(board[0]) - 1
+    #     # print(power)
+    #     for j in i:
+    #         if (j != 0):
+    #             sum += 2 ** power
+    #         power -= 1
+    #     # print(sum)
+    #     sum_list.append(sum)
+
     x_1.append(sum_list)
     data_np = np.array(x_1);
     res = model_1.predict(data_np);
--- tetris/game.py
+++ tetris/game.py
@@ -41,48 +41,6 @@ ACTIONS = [
 IDLE_MAX = 9999
 
 
-# def predict(data):
-#     f = open("logs/samples.json")
-#     j = json.loads("\n".join(f.readlines()))
-#     # Example using NumPy arrays
-#     x = []
-#     y = []
-#     for i in range(0, 259):
-#         sample_x = [int(j[str(i)]['board']), ord(j[str(i)]['piece'])]
-#         sample_y = f"{j[str(i)]['movement']}:{j[str(i)]['rotation']}"
-#         x.append(sample_x)
-#         y.append(sample_y)
-#     X_np = np.array(x)
-#     y_np = np.array(y)
-#     scaler = MinMaxScaler()
-#     scaled_data = scaler.fit_transform(X_np)
-#     print(scaled_data)
-#     # Split data into training and testing sets
-#     X_train, X_test, y_train, y_test = train_test_split(scaled_data, y_np, random_state=42)
-#     # Train a logistic regression model
-#     model = RandomForestClassifier()
-#     model.fit(X_train, y_train)
-
-#     board = str(data[0])
-#     board = json.loads(board)
-#     power = len(board[0]) * len(board) - 1
-#     sum = 0
-#     for i in board:
-#         for j in i:
-#             print(j)
-#             if (j != 0):
-#                 sum += 2 ** power
-#             power -= 1
-#     x.append([sum, ord(data[1])])
-#     data_np = np.array(x);
-#     sc = scaler.fit_transform(data_np)
-#     print(data_np);
-#     print(sc);
-#     return model.predict(sc);
-
-
-
-
 class Gamestate:
     def __init__(self, grid=None, seed=None, rd=None, height=0):
         if seed is None:
@@ -697,7 +655,7 @@ class Game:
                 ##########################################
                 # 534 LOG ACTIONS
                 print("HERE")
-                file = open("logs/test2.txt", "a")
+                file = open("logs/test.txt", "a")
                 file.write("\n\n");
                 file.write(str(self.current_state.next[0]))
                 file.write("\n");
@@ -715,7 +673,7 @@ class Game:
             add_score, done = copy_state.hard_drop()
             ##########################################
             #534 LOG ACTIONS
-            file = open("logs/test2.txt", "a")
+            file = open("logs/test.txt", "a")
             file.write("\n\n");
             file.write(str(self.current_state.next[0]))
             file.write("\n");
@@ -732,7 +690,7 @@ class Game:
         if success:
             ##########################################
             #534 LOG ACTIONS
-            file = open("logs/test2.txt", "a")
+            file = open("logs/test.txt", "a")
             if(action != "down" and action != "soft" and action != "drop" and action != "hold"):
                 file.write(action + ", ")
             ##########################################
@@ -748,7 +706,7 @@ class Game:
                 add_score, done = self.current_state.process_down_collision()
                 ##########################################
                 #534 LOG ACTIONS
-                file = open("logs/test2.txt", "a")
+                file = open("logs/test.txt", "a")
                 file.write("\n\n");
                 file.write(str(self.current_state.next[0]))
                 file.write("\n");
